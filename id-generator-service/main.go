99d9cae37e820cda9e3cf31ef2eef67dafca7b9d
avalokitasharma <avalokitasharma@gmail.com>
2025-07-08 14:15:41 +0530

    init

---
+++ id-generator-service/main.go
@@ -0,0 +1,18 @@
+package idgeneratorservice
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+var counter uint64
+
+func main() {
+	http.HandleFunc("/next", func(w http.ResponseWriter, r *http.Request) {
+		val := atomic.AddUint64(&counter, 1)
+		fmt.Fprintf(w, "%d", val)
+	})
+	fmt.Println("Global counter running on :7001")
+	http.ListenAndServe(":7001", nil)
+}
